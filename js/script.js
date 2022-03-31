4f0db0960f438c86aeed4ef29c1c601476203369
Nicolas <nicolasmoralesj03@gmail.com>
2022-03-31 13:12:26 -0300

    se aplica scroll reveal

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+/* ScrolReveal */
+
+ScrollReveal().reveal('.carousel');
+ScrollReveal().reveal('.footer-col', {delay: 500});
+ScrollReveal().reveal('.icon-whatsapp', {delay: 500});
+ScrollReveal().reveal('.nosotros-container', {delay: 500});
+ScrollReveal().reveal('iframe', {delay: 500});
+ScrollReveal().reveal('.text', {delay: 500});
+ScrollReveal().reveal('.container-section-1', {delay: 500});
+ScrollReveal().reveal('.container-form', {delay: 500});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
